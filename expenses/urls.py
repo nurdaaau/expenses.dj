2fdd55e8bbaee448b45a8615fbd8785e68743111
nurdaaau <nurdaulet.toleukhanov@narxoz.kz>
2023-03-06 19:15:29 +0600

    add project files

---
+++ expenses/urls.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from expenses import settings
+from app import views, views_auth
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('register/', views_auth.registration, name='register'),
+    path('login/', views_auth.login_view, name='login'),
+    path('logout/', views_auth.logout_view, name='logout'),
+    path('account/', views.account, name='account'),
+    path('create/', views.create_view, name='create'),
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
